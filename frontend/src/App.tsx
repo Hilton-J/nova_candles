2075d82ee68faf719b5618f4ca8d8b9d34f9278c
Hilton-J <lhmajola@outlook.com>
2025-02-26 16:05:08 +0200

    Busy with View Product component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router";
+import { Routes, Route, useLocation } from "react-router";
 import AdminLayout from "./layouts/AdminLayout";
 import MainLayout from "./layouts/MainLayout";
 import LandingPage from "./pages/LandingPage";
@@ -6,20 +6,34 @@ import About from "./components/About";
 import Login from "./components/Login";
 import Shop from "./components/Shop";
 import ViewProduct from "./components/ViewProduct";
+import { useEffect } from "react";
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
 
 function App() {
   return (
-    <Routes>
-      <Route path='/admin' element={<AdminLayout />}></Route>
-      <Route path='/login' element={<Login />} />
-      <Route path='/' element={<MainLayout />}>
-        <Route index element={<LandingPage />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/shop' element={<Shop />} />
-        {/* <Route path='/products/:id' element={<ViewProduct />} /> */}
-        <Route path='/products/:id/:size' element={<ViewProduct />} />
-      </Route>
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path='/admin' element={<AdminLayout />}></Route>
+        <Route path='/login' element={<Login />} />
+        <Route path='/' element={<MainLayout />}>
+          <Route index element={<LandingPage />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/shop' element={<Shop />} />
+          {/* <Route path='/products/:id' element={<ViewProduct />} /> */}
+          <Route path='/products/:id/:size' element={<ViewProduct />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
--- frontend/src/components/ViewProduct.tsx
+++ frontend/src/components/ViewProduct.tsx
@@ -8,7 +8,9 @@ const ViewProduct = () => {
   const { id, size } = useParams<{ id: string; size: string }>();
   const [productSize, setProductSize] = useState<string | undefined>(size);
   const { data, isLoading } = useGetProductByIdQuery(id || "");
-  const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(
+    undefined
+  );
 
   const product = data?.find(
     (item: IProduct) => item.size.toLowerCase() === productSize?.toLowerCase()
@@ -24,49 +26,53 @@ const ViewProduct = () => {
       {isLoading ? (
         <Loader loading={isLoading} />
       ) : (
-        <div className='grid md:grid-cols-[35%_65%] h-[60%] gap-10'>
-          <div className='grid grid-cols-[10%_88%] gap-[2%]'>
+        <div className='grid md:grid-cols-[35%_60%] gap-[5%] w-[90%] mx-auto'>
+          {/* Left Side: Product Pictures */}
+          <div className='grid grid-rows-subgrid row-span-2 gap-6 h-full'>
+            {/* Main Image */}
+            <div className='h-[35rem]'>
+              <img
+                src={selectedImage}
+                alt={product?.productName}
+                className='size-full object-cover'
+              />
+            </div>
+
             {/* Thumbnails */}
-            <div className='flex flex-col gap-2'>
+            <div className='flex gap-2'>
               {product?.images?.map((img, index) => (
                 <img
                   key={index}
                   src={img}
                   alt={`Thumbnail ${index}`}
-                  className={`size-16 object-cover cursor-pointer ${
+                  className={`size-18 object-cover cursor-pointer ${
                     selectedImage === img ? "brightness-100" : "brightness-50"
                   }`}
                   onClick={() => setSelectedImage(img)}
                 />
               ))}
             </div>
-
-            {/* Main Image */}
-            <img
-              src={selectedImage}
-              alt={product?.productName}
-              className='size-full object-cover'
-            />
           </div>
 
           {/* Right Side: Product Details */}
           <div className='flex flex-col'>
-            <div>
+            <div className='xl:text-[2.7rem] 2xl:text-[4rem]'>
               <h1>
                 {product?.productName} -
                 <span className='capitalize'> {product?.size}</span>
               </h1>
-              <p>R{product?.price.toFixed(2)}</p>
+              <p className='font-bold'>R{product?.price.toFixed(2)}</p>
             </div>
 
             {/* Display Size Selector Only If Product Size is Valid */}
             {["small", "medium", "large"].includes(product?.size || "") && (
               <div>
-                <h2>
-                  Select size:{" "}
-                  <span className='capitalize'>{product?.size}</span>
+                <div className='border border-black/20 w-[90%] mb-3'></div>
+                <h2 className='xl:text-[1.5rem] '>
+                  Select size:
+                  <span className='capitalize'> {product?.size}</span>
                 </h2>
-                <div>
+                <div className='space-x-2'>
                   {["small", "medium", "large"].map((s) => (
                     <button
                       key={s}
@@ -74,8 +80,8 @@ const ViewProduct = () => {
                       className={`${
                         productSize === s
                           ? "bg-accent text-white"
-                          : "bg-secondary "
-                      } border border-black/20 hover:bg-transparent hover:text-black text-black py-1 px-3 cursor-pointer ml-auto`}
+                          : "bg-transparent "
+                      } border border-black/20 hover:bg-secondary hover:text-black text-black py-1 px-3 cursor-pointer ml-auto`}
                     >
                       {s.charAt(0).toUpperCase() + s.slice(1)}
                     </button>
@@ -83,12 +89,20 @@ const ViewProduct = () => {
                 </div>
               </div>
             )}
-            <p>
-              {/* Is a reminiscent of warm beach days, high notes of lemon palm and
-              floral freesia to draw into the midst of warm coconut and sweet
-              sugarcane. It ends with a luxury vetiver and hint of vanilla */}
-              {product?.description}
-            </p>
+
+            <div className='border border-black/20 w-[90%] my-3'></div>
+
+            <p className='w-[90%]'>{product?.description}</p>
+            <div>
+              <input
+                type="number"
+                defaultValue={1}
+                className='border border-black/20 py-1 px-3'
+              />
+              <button className='mt-3 bg-accent text-white border border-black/20 hover:bg-secondary hover:text-black py-1 px-3 mr-auto cursor-pointer h-fit'>
+                Add To Cart
+              </button>
+            </div>
           </div>
         </div>
       )}
