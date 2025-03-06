52ead120521b61b1b4f4e1e227898ba4bcf54008
Hilton-J <140448818+Hilton-J@users.noreply.github.com>
2025-03-06 22:16:47 +0200

    BAckend change

--- backend/routes/userRoute.mjs
+++ backend/routes/userRoute.mjs
@@ -1,11 +1,12 @@
 import express from 'express';
 import { registerUser, login, getAllUsers, logout, getUserById, deleteUser, updateUser } from '../controllers/userController.mjs';
 import { protect, authorizeRoles } from '../middleware/authMiddleware.mjs'
+import validateLogin from '../middleware/validator/validateLogin.mjs';
 
 const router = express.Router();
 
 router.post('/register', registerUser);
-router.post('/login', login);
+router.post('/login', validateLogin, login);
 router.post('/logout', logout);
 router.route('/profile').get(protect, getUserById).put(protect, updateUser);
 router.get('/', protect, authorizeRoles('admin'), getAllUsers);
--- backend/schemas/authSchema.mjs
+++ backend/schemas/authSchema.mjs
@@ -4,14 +4,16 @@ export const emailSchema = z.string().email('Invalid email');
 
 const passwordSchema = z.string().min(6, 'Password must be at least 6 characters');
 
-export const loginShcema = z.object({
+export const loginSchema = z.object({
   email: emailSchema,
   password: passwordSchema,
 });
 
-export const registerSchema = loginShcema.extend({
+
+export const registerSchema = loginSchema.extend({
   confirmPassword: passwordSchema,
-  role: z.enum(['customer', 'admin']).default('customer')
+  role: z.enum(['customer', 'admin']).default('customer'),
+
 })
   .refine((data) => data.password === data.confirmPassword, {
     message: 'Password do not match',
